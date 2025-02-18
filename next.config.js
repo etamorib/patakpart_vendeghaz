fc49e667acabbb569b68dc777e4506e0322ba19b
etamorib <partiz4nn@gmail.com>
2025-02-18 01:04:27 +0100

    update gh pages deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
+
 const nextConfig = {
   //output: 'export'
   images: {
     unoptimized: true
-  }
+  },
+  assetPrefix: isProd ? '/patakpart_vendeghaz/' : '',
+  basePath: isProd ? '/patakpart_vendeghaz' : '',
+  output: 'export'
 }
 
 module.exports = nextConfig
